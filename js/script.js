5dba151cb80f8feeffe2f90f6bbc11f79a1fecfb
Juliano Silveira <108852825+iJulians@users.noreply.github.com>
2022-07-28 01:18:59 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+import Countdown from './countdown.js'
+
+const tempoParaSabado = new Countdown('30 july 2022 18:30:00 GMT-0300')
+
+const tempos = document.querySelectorAll('[data-time]')
+
+function mostrarTempo() {
+  tempos.forEach((tempo, index) => {
+    tempo.innerHTML = tempoParaSabado.total[index]
+  })
+}
+
+mostrarTempo()
+setInterval(mostrarTempo, 1000)
