6bb673d553b1c2286d3bc5c984e782e685ccbd1c
Juliano Silveira <108852825+iJulians@users.noreply.github.com>
2022-08-13 16:08:59 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import Countdown from './countdown.js'
 
-const tempoParaSabado = new Countdown('05 August 2022 18:30:00 GMT-0300')
+const tempoParaSabado = new Countdown('13 August 2022 18:15:00 GMT-0300')
 
 const tempos = document.querySelectorAll('[data-time]')
 
